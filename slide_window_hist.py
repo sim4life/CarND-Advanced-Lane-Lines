151df09c3166f07e72d122ab10b9a6929b121c82
Shah Ismail <sim4life@yahoo.com>
2017-03-24 19:28:59 +0000

    Writeup WIP 2

--- slide_window_hist.py
+++ slide_window_hist.py
@@ -76,25 +76,10 @@ def slide_window(binary_warped):
     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-    # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 9))
-    # f.tight_layout()
-    #
-    # ax1.imshow(binary_warped, cmap='gray')
-    # ax1.set_title('Binary warped', fontsize=10)
-    #
-    # ax2.imshow(out_img)
-    # ax2.set_title('Sliding window histogram', fontsize=10)
-    # ax2.plot(left_fitx, ploty, color='yellow')
-    # ax2.plot(right_fitx, ploty, color='yellow')
-    #
-    # plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.)
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
-    # plt.show()
     return leftx, lefty, rightx, righty, left_fit, right_fit
 
 def skip_slide_window(binary_warped, left_fit, right_fit):
-    # Assume you now have a new warped binary image
+    # Assuming we now have a new warped binary image
     # from the next frame of video (also called "binary_warped")
     # It's now much easier to find line pixels!
     nonzero = binary_warped.nonzero()
@@ -104,46 +89,24 @@ def skip_slide_window(binary_warped, left_fit, right_fit):
     left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
     right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
 
-    # Again, extract left and right line pixel positions
+    # Again, extracting left and right line pixel positions
     leftx = nonzerox[left_lane_inds]
     lefty = nonzeroy[left_lane_inds]
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds]
-    # Fit a second order polynomial to each
+    # Fitting a second order polynomial to each
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-    # Generate x and y values for plotting
-    # ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-    # left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-    # right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    # Generating x and y values for plotting
     left_fitx, right_fitx = fit_curve(binary_warped, left_fit, right_fit)
 
-    # Create an image to draw on and an image to show the selection window
+    # Creating an image to draw on and an image to show the selection window
     out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
     window_img = np.zeros_like(out_img)
-    # Color in left and right line pixels
+    # Coloring in left and right line pixels
     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-    # # Generate a polygon to illustrate the search window area
-    # # And recast the x and y points into usable format for cv2.fillPoly()
-    # left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-    # left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
-    # left_line_pts = np.hstack((left_line_window1, left_line_window2))
-    # right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-    # right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
-    # right_line_pts = np.hstack((right_line_window1, right_line_window2))
-
-    # # Draw the lane onto the warped blank image
-    # cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
-    # cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-    # result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-    # plt.imshow(result)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
-    # plt.show()
     return leftx, lefty, rightx, righty, left_fit, right_fit
 
 def fit_curve(binary_warped, left_fit, right_fit):
